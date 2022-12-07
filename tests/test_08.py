5cebc91ef508510e4b542a18505967c449deff94
Allan Kent <allan.kent@elerian.ai>
2022-12-07 17:52:05 +0200

    day 8 prep

---
+++ tests/test_08.py
@@ -0,0 +1,16 @@
+import unittest
+import src.day_08 as day_08
+
+
+class TestDay_08(unittest.TestCase):
+    input = """"""
+
+    def test_part1(self):
+        expected = None
+        result = day_08.part1(self.input)
+        self.assertEqual(result, expected)
+
+    def test_part2(self):
+        expected = None
+        result = day_08.part2(self.input)
+        self.assertEqual(result, expected)
